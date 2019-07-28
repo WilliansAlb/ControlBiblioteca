26b744b3a9a3032814efcb4b6fa189cd0da8a6e0
WilliansAlb <williansalbertolpez@hotmail.com>
2019-07-28 01:31:27 -0600

    Inicio de la práctica en Git

---
+++ src/controlbiblioteca/ControlBiblioteca.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlbiblioteca;
+
+/**
+ *
+ * @author willians
+ */
+public class ControlBiblioteca {
+   
+    public static void main(String args[]){
+        Biblioteca nueva = new Biblioteca();
+        nueva.setVisible(true);
+    }
+}
